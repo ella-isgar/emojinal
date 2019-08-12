33bc15d25e51fa7c54d5a00bedf5260186bb2167
Ella Isgar <Ella@Apples-MacBook-Pro-63.local>
2019-08-12 10:12:34 -0400

    Complete Iteration 0

---
+++ emojinal/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  emojinal
+//
+//  Created by Ella Isgar on 8/12/19.
+//  Copyright © 2019 Ella Isgar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    //if happy emoji clicked...
+    @IBAction func happy(_ sender: Any) {
+        
+    }
+    
+    //if sad emoji clicked...
+    @IBAction func sad(_ sender: Any) {
+        
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+
+}
+
